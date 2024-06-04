834832886250982a7d832d64b56c014ddb16b343
Binson Sam Thomas <binsonin95@gmail.com>
2024-06-04 15:18:46 +0100

    Adding HPC Codes

---
+++ HPC_Codes/downsample.py
@@ -0,0 +1,38 @@
+import os
+import numpy as np
+import shutil
+
+def downsample_dataset(original_folder, output_folder):
+    
+    dataset_info = []
+    for root, _, files in os.walk(original_folder):
+        for file in files:
+            if file.endswith('.npy'):
+                print(f"Processing {file} ")
+                file_path = os.path.join(root, file)
+                data = np.load(file_path, allow_pickle=True).item()
+                label = data['label']
+                dataset_info.append((file_path, label))
+                
+    fall_samples = [info for info in dataset_info if 1 <= info[1] <= 5]
+    non_fall_samples = [info for info in dataset_info if info[1] > 5]
+    
+    num_falls = len(fall_samples)
+    print(num_falls)
+    downsampled_non_falls_indices = np.random.choice(len(non_fall_samples), size = num_falls, replace = False)
+    downsampled_non_falls = [non_fall_samples[i] for i in downsampled_non_falls_indices]
+    
+    for file_path, _ in fall_samples + list(downsampled_non_falls):
+        print(f"Creating file {file_path} ")
+        relative_path = os.path.relpath(file_path, original_folder)
+        new_path = os.path.join(output_folder, relative_path)
+        
+        os.makedirs(os.path.dirname(new_path), exist_ok=True)
+        
+        shutil.copyfile(file_path, new_path)
+        
+if __name__ == "__main__":
+    original_folder = 'smt_repo/processed_ds/Canny_only_resized'
+    output_folder = 'smt_repo/processed_ds/balanced_Canny'
+    
+    downsample_dataset(original_folder, output_folder)
